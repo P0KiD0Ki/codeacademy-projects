c255029de5296a64ad950f0211c771a72486582c
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2022-02-26 10:40:53 -0500

    added variables to project

--- moneymaker.cs
+++ moneymaker.cs
@@ -6,7 +6,18 @@ namespace MoneyMaker
   {
     public static void Main(string[] args)
     {
-      Console.WriteLine("Welcome to Money Maker!");
+      Console.WriteLine("Welcome to Money Maker!\n");
+      Console.WriteLine("Enter an amount to convert to coins:");
+      string amount = Console.ReadLine();
+      double amountAsDouble = Convert.ToDouble(amount);
+
+      Console.WriteLine($"{amountAsDouble} cents is equal to...");
+
+      double gold = 10;
+      double silver = 5;
+  
+  
+  //Console.WriteLine($"{gold} \n {silver}");
       
     }
   }
