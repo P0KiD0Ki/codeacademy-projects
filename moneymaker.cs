6d4d6e4af24a691d06396dd9d72fa331a1c2652e
Perla Oki <76271424+P0KiD0Ki@users.noreply.github.com>
2022-02-26 11:23:26 -0500

    changed to US currency

--- moneymaker.cs
+++ moneymaker.cs
@@ -6,18 +6,35 @@ namespace MoneyMaker
   {
     public static void Main(string[] args)
     {
+      //Data gathering
       Console.WriteLine("Welcome to Money Maker!\n");
       Console.WriteLine("Enter an amount to convert to coins:");
-      string amount = Console.ReadLine();
-      double amountAsDouble = Convert.ToDouble(amount);
+      string amountAsString = Console.ReadLine();
+      double amount = Math.Floor(Convert.ToDouble(amountAsString));
 
-      Console.WriteLine($"{amountAsDouble} cents is equal to...");
+      Console.WriteLine($"\n{amount} cents is equal to...\n");
 
-      double gold = 10;
-      double silver = 5;
+      //TODO: add logic that converts any input over 99 cents to dollars.
+
+      //quarter conversion and remainder
+      double quarter = 25;
+      double quarters = Math.Floor(amount / quarter);
+      double remainder = amount % quarter;
+
+      //dime conversion and remainder
+      double dime = 10;
+      double dimes = Math.Floor(remainder / dime);
+      remainder = remainder % dime;
+
+      //nickle conversion and remainder
+      double nickle = 5;
+      double nickles = Math.Floor(remainder / nickle);
+
+      //pennies
+      remainder = remainder % nickle;
   
   
-  //Console.WriteLine($"{gold} \n {silver}");
+  Console.WriteLine($"Quarters: {quarters}\nDimes: {dimes}\nNickles: {nickles}\nPennies: {remainder}");
       
     }
   }
